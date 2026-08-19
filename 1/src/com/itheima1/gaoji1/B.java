da6bbbe3c9d62713cf2bbea9520036c97dd93d8a
surface <surface@cc.com>
2026-08-19 14:03:41 +0800

    1

---
+++ 1/src/com/itheima1/gaoji1/B.java
@@ -0,0 +1,5 @@
+package com.itheima1.gaoji1;
+
+public class B {
+
+}
---
+++ 1/src/com/itheima1/hello/gaoJi1.java
@@ -0,0 +1,12 @@
+package com.itheima1.hello;
+
+public class gaoJi1 {
+    public static void main(String[] args) {
+        System.out.println("1 继承");
+        // 继承：Java中extends关键字 可以让一个类和另一个类建立父子关系
+        // 继承特点：子类能够继承父类非私有成员（成员变量 成员方法）
+        // 继承后对象的创建：子类的对象是由子类、父类共同完成的。
+
+
+    }
+}
